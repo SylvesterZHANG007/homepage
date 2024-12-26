5e2b2a2412fd9ede409f4cdbec82b1adad60d85a
SylvesterZHANG007 <sylvester.zhang7@gmail.com>
2024-12-26 00:14:05 -0500

    Update script.js
    Signed-off-by: SylvesterZHANG007 <sylvester.zhang7@gmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
 
-    // 监听滚动事件
     window.addEventListener('scroll', () => {
         if (window.scrollY > 100) {
-            body.classList.add('scrolled'); // 滚动时显示其他内容
+            body.classList.add('scrolled');
         } else {
-            body.classList.remove('scrolled'); // 回到顶部时只显示大字
+            body.classList.remove('scrolled');
         }
     });
 });
