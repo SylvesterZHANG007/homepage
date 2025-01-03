6a296a63a737cca19b50875c9191188d7312d854
SylvesterZHANG007 <sylvester.zhang7@gmail.com>
2025-01-03 16:05:21 -0500

    Update script.js
    Signed-off-by: SylvesterZHANG007 <sylvester.zhang7@gmail.com>

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
-            body.classList.add('scrolled');
+        if (window.scrollY > 200) { // 设置切换导航栏和正文的滚动阈值
+            body.classList.add('scrolled'); // 滚动后切换到导航栏模式
         } else {
-            body.classList.remove('scrolled');
+            body.classList.remove('scrolled'); // 初始模式
         }
     });
 });
